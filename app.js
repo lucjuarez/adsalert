9b54283aae003c8425d3761e7e7695f873e3cefb
lucjuarez <67291392+lucjuarez@users.noreply.github.com>
2026-03-27 15:55:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -161,6 +161,8 @@ cron.schedule("* * * * *", async () => {
 });
 
 // 🚀 SERVER
-app.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
